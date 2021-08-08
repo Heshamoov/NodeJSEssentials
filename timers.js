e0ab768307d44ae7bf2043504bad5818e810f5b2
heshamoov <heshamoov90@gmail.com>
2021-08-08 13:56:04 +0400

    Essential Training

--- timers.js
+++ timers.js
@@ -11,11 +11,13 @@ const incTime = () => {
 };
 
 const timerFinished = () => {
-  clearInterval(interval);
+  clearInterval(interval); // Stopping the interval
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
   process.stdout.write(`Done!`);
 };
 
+// To prevent the interval from running infinitly, we need to clear the interval
+// and in order to clean the intervel w should have assign a variable for the setInterval
 const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
